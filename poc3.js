7502ae2645f67ca2604e55e38ef88e4140ab4fe2
Sarp Dora Yönden <69794292+sarpdora23@users.noreply.github.com>
2024-06-22 23:29:23 +0300

    Update and rename poc2.js to poc3.js

---
+++ poc3.js
@@ -0,0 +1,21 @@
+async function fetchGoogle() {
+    try {
+        const response = await fetch('https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.text();
+        console.log(data);
+    } catch (error) {
+        console.error('Fetch error: ', error);
+    }
+}
+
+fetchGoogle();
