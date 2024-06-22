1009288f30da6bfb2a248ee3e95a34d461426956
Sarp Dora Yönden <69794292+sarpdora23@users.noreply.github.com>
2024-06-23 00:10:17 +0300

    Update and rename poc10.js to poc11.js

--- poc11.js
+++ poc11.js
@@ -1,21 +1,22 @@
 function fetchGoogle(ctx, req, res) {
-    try {
-        const response = fetch('https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
-
+    fetch('https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html',
+        },
+    })
+    .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-
-        const data = await response.text();
+        return response.text();
+    })
+    .then(data => {
         res.status(200).send(data);  // Başarılı yanıtı gönder
-    } catch (error) {
+    })
+    .catch(error => {
         res.status(500).send('Fetch error: ' + error.message);  // Hata durumunda yanıt gönder
-    }
+    });
 }
 
 module.exports = fetchGoogle;
